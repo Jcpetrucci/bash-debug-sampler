1e3a281d6dd60f1c53bbe3220766ab3c1956277b
John C. Petrucci <Jcpetrucci@users.noreply.github.com>
2014-11-18 13:18:47 -0600

    explain manual multi-flag debugs

--- debug-sampler.sh
+++ debug-sampler.sh
@@ -2,10 +2,14 @@
 # Create: 2013-07-03 John C. Petrucci
 # http://johncpetrucci.com
 # Purpose: Educational.  Generate sample data for every possible kernel debug of Check Point Firewall kernel (module).
-# Usage: Takes a single optional argument; a file name containing TYPE, MODULE and FLAG names in the format of
-#	  |+ 'type module flag' with one entry per line.  TYPE is one of {fw1|fwaccel|sim}.
-#	  |+ If no argument is given, automatic discovery will be used
-#	  \+ to determine all of the available modules and flags.
+# Usage: Takes a single optional argument; a file name containing TYPE, MODULE and FLAG names in the format of:
+#	  TYPE MODULE FLAG [FLAG [FLAG]]...
+#	  ..separated by newline.  TYPE is one of {fw1|fwaccel|sim}.
+#	  Multiple FLAGs can be given per debug line (e.g. `fw1 fw conn drop`, where 'conn' & 'drop' are flags).
+#	  Multi-flag debugs are only possible when a manual file is specified, as the automatic discovery mechanism
+#	  simply iterates through each discovered flag one by one.
+#
+#	  If no argument is given, automatic discovery will be used to determine all of the available modules and flags.
 # Examples: 
 #		./debug-sampler.sh
 #		./debug-sampler.sh modules.txt
